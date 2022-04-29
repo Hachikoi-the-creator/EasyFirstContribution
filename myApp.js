69a0f5a0dea28330e31913d9c246d3aa46fe6022
Hachikoi-Marine <75096372+Hachikoi-Marine@users.noreply.github.com>
2022-04-29 18:46:19 -0500

    added some challenges

---
+++ myApp.js
@@ -0,0 +1,12 @@
+//Console.log the sum of number is in the array
+const arr = [1,2,3,7,6,10,89,3]
+const arr = []//return -1
+
+//Invert the case of all the letters in the string
+let str = ''//return -1
+let str = 'GitHub'
+let str = 'StarDust'
+
+//Return the value of the key 'country'
+const a = {  continent: 'Asia',  country: 'Japan'}
+const b = {  continent: 'America',  country: 'México'}
